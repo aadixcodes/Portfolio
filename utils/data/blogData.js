fef1853bc418b3a982953130b88da32bda2e4d67
iampateladitya <adityapatel77045@gmail.com>
2024-08-26 21:54:05 +0530

    Portfolio Done

--- utils/data/blogData.js
+++ utils/data/blogData.js
@@ -10,7 +10,7 @@ export const blogData = [
         id: 1,
         name: 'UniPrep - Exam Preparation Tool',
         link: `https://uniprep-edu.vercel.app/`,
-        date: '02/02/2024 - 02/02/2024',
+        date: '30/June/2024 - 25/Auguest/2024',
         description: `UniPrep is go-to platform for academic success.
 UniPrep designed to enhance your university study experience. UniPrep provide curated important questions, previous years' exam papers, and opportunities to buy and sell handwritten notes.`,
         image: uniprep,
@@ -18,7 +18,7 @@ UniPrep designed to enhance your university study experience. UniPrep provide cu
     {
         id: 2,
         name: 'Assets Management - IMC Indore',
-        date: '02/02/2024 - 02/02/2024',
+        date: '26/July/2024 - 28/July/2024',
         link : `https://imc-hackndore.vercel.app/`,
         description: 'The Assets Management System is a robust solution tailored to assist municipal corporations in efficiently managing both movable and non-movable assets. By providing a centralized platform.',
         image: imc,
@@ -26,7 +26,7 @@ UniPrep designed to enhance your university study experience. UniPrep provide cu
     {
         id: 3,
         name: 'Navgo - Bus Tracking WebApp',
-        date: '02/02/2024 - 02/02/2024',
+        date: '18/May/2024 - Present',
         link : `https://navgo-svvv.vercel.app/`,
         description: 'NavGo is a dedicated bus tracking web app designed specifically for college students. With NavGo, students can live track their college buses in real time.',
         image: navgo,
@@ -34,7 +34,7 @@ UniPrep designed to enhance your university study experience. UniPrep provide cu
     {
         id: 4,
         name: 'SvvvInvento - Inventory Management System',
-        date: '02/02/2024 - 02/02/2024',
+        date: '26/April/2024 - 02/June/2024',
         link : `https://github.com/aadixcodes/SvvvInvento-Inventory-Web-App`,
         description: `SVVV Invento is a comprehensive web application designed to efficiently manage and track electronic inventory items such as laptops, desktops, and more within educational institutions.`,
         image: svvvinvento,
@@ -42,7 +42,7 @@ UniPrep designed to enhance your university study experience. UniPrep provide cu
     {
         id: 5,
         name: 'VoteSphere - Online Voting System',
-        date: '02/02/2024 - 02/02/2024',
+        date: '30/Dec/2023 - 31/Dec/2023',
         link : `https://votesphere.vercel.app/`,
         description: `VoteSphere is an online voting platform designed to streamline and secure the voting process. Easily create, manage, and participate in votes and elections with confidence and convenience.`,
         image: votesphere,
@@ -50,7 +50,7 @@ UniPrep designed to enhance your university study experience. UniPrep provide cu
     {
         id: 5,
         name: 'Abhyudaya - Coding club Portfolio',
-        date: '02/02/2024 - 02/02/2024',
+        date: '02/Feb/2024 - 01/Mar/2024',
         link : `https://abhyudayacodingclub.vercel.app/`,
         description: `The Abhyudaya website is the official platform for the coding club of Vaishnav University. It serves as a hub for students passionate about coding, offering resources, event updates, and a space to showcase projects.`,
         image: abhyudaya,
